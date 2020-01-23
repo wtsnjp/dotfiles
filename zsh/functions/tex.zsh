aaa7c5b78ea589e7e20986a2e1a1195ee724f9fa
Takuto ASAKURA <wtsnjp@gmail.com>
2020-01-23 20:14:45 +0900

    tex.zsh: implement the texsw command

--- zsh/functions/tex.zsh
+++ zsh/functions/tex.zsh
@@ -3,8 +3,90 @@
 # Website: https://wtsnjp.com
 # Source: https://github.com/wtsnjp/dotfiles
 
-# warn and abort reading if TeX Live isn't installed
+## texsw (only for main macOS)
+function __texsw_init() {
+  local tl_dir="/usr/local/texlive"
+  local dev_tl_dir="$HOME/repos/tug.org/Master"
+
+  # check the main tl dir
+  if [ ! -d $tl_dir ]; then
+    return 1
+  fi
+
+  # search texlives
+  typeset -g -A texsw_texlives
+  for tl in $(find "$tl_dir" -maxdepth 1 -type d \
+    | grep -e "$tl_dir/\d\d\d\d"); do
+    texsw_texlives[$(basename $tl)]="$tl"
+  done
+
+  # default: the latest texlive
+  local latest=$(echo ${(@O)texsw_texlives} | cut -d' ' -f 1)
+  export TEXSW_CURRENT_TEXLIVE=$latest
+  __add_path "$TEXSW_CURRENT_TEXLIVE/bin/x86_64-darwin"
+
+  # add dev TL
+  if [ -d $dev_tl_dir ]; then
+    texsw_texlives[dev]="$dev_tl_dir"
+  fi
+
+  # activate internal functions
+  __texsw_functions
+}
+
+function __texsw_functions() {
+  function __texsw_list() {
+    local short
+    local texsw_status
+
+    for tl_name in ${(k)texsw_texlives}; do
+      local tl_path=$texsw_texlives[$tl_name]
+      if [ "$tl_path" = "$TEXSW_CURRENT_TEXLIVE" ]; then
+        texsw_status="* "
+      else
+        texsw_status="  "
+      fi
+      echo "$texsw_status$tl_name\t($tl_path)"
+    done
+  }
+
+  function __texsw_switch() {
+    local prev_tl
+
+    if [[ -n "${texsw_texlives[(i)$1]}" ]]; then
+      prev_tl="$TEXSW_CURRENT_TEXLIVE"
+      export TEXSW_CURRENT_TEXLIVE=$texsw_texlives[$1]
+    else
+      echo "texsw: TeX Live $1 does not exist."
+      return 1
+    fi
+
+    # remove previous TL and add new TL to the path
+    export PATH=$(echo -n $PATH | tr ':' '\n' \
+      | sed "\@$prev_tl/bin/x86_64-darwin@d" | tr '\n' ':')
+    __add_path "$TEXSW_CURRENT_TEXLIVE/bin/x86_64-darwin"
+
+    # report
+    echo "texsw: switch to: $TEXSW_CURRENT_TEXLIVE"
+    echo "texsw:      from: $prev_tl"
+  }
+}
+
+## preparation
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if __texsw_init; then
+    function texsw() {
+      if [ "$1" = "-l" ]; then
+        __texsw_list
+      else
+        __texsw_switch $1
+      fi
+    }
+  fi
+fi
+
 if ! __is_cmd kpsewhich; then
+  # no TL: warn and abort
   echo "WARNING: Script $0 only works with TeX Live!"
   return
 fi
@@ -40,7 +122,7 @@ function kpse() {
 ## __kpse_src <file>
 function __kpse_src() {
   local target
-  local exts=("" ".dtx" ".sty")
+  local exts=("" ".dtx" ".sty" ".cls")
 
   for ext in "$exts[@]"; do
     target=$(kpsewhich "$1$ext")
