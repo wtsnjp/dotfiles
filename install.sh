70300decf84077d6cfe39ee7c6ed523d1596905b
Takuto ASAKURA <wtsnjp@gmail.com>
2021-06-14 13:06:22 +0900

    better install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
-REPO_ROOT="$(pwd)"
+REPO_ROOT="$(cd $(dirname $0); pwd)"
 
-ln -s "$REPO_ROOT/gitconfig" "$HOME/.gitconfig"
-ln -s "$REPO_ROOT/gitignore.global" "$HOME/.gitignore.global"
-ln -s "$REPO_ROOT/vimrc" "$HOME/.vimrc"
-ln -s "$REPO_ROOT/zshrc" "$HOME/.zshrc"
+ln -s $REPO_ROOT/gitconfig $HOME/.gitconfig
+ln -s $REPO_ROOT/gitignore.global $HOME/.gitignore.global
+ln -s $REPO_ROOT/vimrc $HOME/.vimrc
+ln -s $REPO_ROOT/zshrc $HOME/.zshrc
+
+ZSH_FUNC_DIR="$HOME/.zsh/functions"
+mkdir -p $ZSH_FUNC_DIR
+ln -s $REPO_ROOT/zsh/functions/utility.zsh $ZSH_FUNC_DIR
